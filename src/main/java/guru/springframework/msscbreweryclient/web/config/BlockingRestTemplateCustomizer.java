c6f8e240c22c228b56b17536ce814fc87e41e1e6
jsw060953 <john.stanford.white@gmail.com>
2024-07-08 15:45:03 -0400

    finally got the @Value annotation to work

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -1,53 +1,62 @@
+
 package guru.springframework.msscbreweryclient.web.config;
 
-import lombok.Value;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-    /**
-     * Created by jt on 2019-08-08.
-     */
-    @Component
-    public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
-        @Value( "${http.connection.timeout}" )
-        private int requestTimeout;
+/**
+ * Created by jt on 2019-08-08.
+ */
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
-        @Value ("${http.connection.maxconections}")
-        private int maxConnections;
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxTotalConnetions;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
 
-        public ClientHttpRequestFactory clientHttpRequestFactory(){
-            PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-            //connectionManager.setMaxTotal(100);
-            connectionManager.setMaxTotal(maxConnections);
-            connectionManager.setDefaultMaxPerRoute(20);
+    public BlockingRestTemplateCustomizer(@Value("${sfg.maxtotalconnections}") Integer maxTotalConnections,
+                                          @Value("${sfg.defaultmaxtotalconnections}") Integer defaultMaxTotalConnetions,
+                                          @Value("${sfg.connectionrequesttimeout}")Integer connectionRequestTimeout,
+                                          @Value("${sfg.sockettimeout}")Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxTotalConnetions = defaultMaxTotalConnetions;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
 
-            // 3000
-            RequestConfig requestConfig = RequestConfig
-                    .custom()
-                    .setConnectionRequestTimeout(requestTimeout)
-                    .setSocketTimeout(requestTimeout)
-                    .build();
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnetions);
 
-            CloseableHttpClient httpClient = HttpClients
-                    .custom()
-                    .setConnectionManager(connectionManager)
-                    .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
-                    .setDefaultRequestConfig(requestConfig)
-                    .build();
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
 
-            return new HttpComponentsClientHttpRequestFactory(httpClient);
-        }
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
 
-        @Override
-        public void customize(RestTemplate restTemplate) {
-            restTemplate.setRequestFactory(this.clientHttpRequestFactory());
-        }
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
 }
